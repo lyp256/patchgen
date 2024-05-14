5ffca6cb30e001196d59bddb534b4a2209026761
lyp256 <lyp256@qq.com>
2024-05-14 15:04:48 +0800

    init project

---
+++ README.md
@@ -0,0 +1,22 @@
+# Patchgen
+
+generate kubernetes resource PATCH
+
+## Install
+
+go install github.com/lyp256/patchgen # go version 1.16+
+
+## Usage
+
+```shell
+patchgen [raw file] [update file]
+  -t, --type string   merge|strategic (default "merge")
+ ```
+
+### example
+
+```shell
+> patchgen .\raw.json .\new.json
+{"spec":{"template":{"spec":{"volumes":[{"hostPath":{"path":"/proc","type":""},"name":"proc"},{"hostPath":{"path":"/sys","type":""},"name":"sys"},{"hostPath
+":{"path":"/","type":""},"name":"root"},{"configMap":{"name":"node-export-web-config"},"name":"web-config"}]}}}}
+```
---
+++ patchgen.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/pflag"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	serializeryaml "k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+var decUnstructured = serializeryaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
+
+func usage() {
+	_, exe := filepath.Split(os.Args[0])
+	fmt.Fprintf(os.Stderr, "usage: %s [raw file] [update file]\n", exe)
+	pflag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	t := pflag.StringP("type", "t", "strategic", "merge|strategic")
+	pflag.Parse()
+
+	if len(pflag.Args()) != 2 {
+		usage()
+	}
+	raw, err := getObject("raw.json")
+	if err != nil {
+		panic(err)
+		return
+	}
+	update, err := getObject("new.json")
+	if err != nil {
+		panic(err)
+		return
+	}
+	var patch client.Patch
+	switch strings.ToLower(*t) {
+	case "merge":
+		patch = client.MergeFrom(raw)
+	case "strategic":
+		patch = client.StrategicMergeFrom(raw)
+	default:
+		logrus.Errorf("unknown type: %s", *t)
+		usage()
+	}
+	data, err := patch.Data(update)
+	if err != nil {
+		panic(err)
+	}
+	_, _ = os.Stdout.Write(data)
+}
+
+func getObject(filename string) (client.Object, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	meta, _, err := decUnstructured.Decode(data, nil, &metaV1.PartialObjectMetadata{})
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := scheme.Scheme.New(meta.GetObjectKind().GroupVersionKind())
+	if err != nil {
+		return nil, err
+	}
+	out, _, err = decUnstructured.Decode(data, nil, out)
+	if err != nil {
+		return nil, err
+	}
+	return out.(client.Object), err
+}
